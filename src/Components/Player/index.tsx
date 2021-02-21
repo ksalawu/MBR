57db668d5df228291ab4fb2225db384ada762a43
Kieran Salawu <kieransalawu@kierans-mbp.home>
2021-02-21 17:06:23 +0000

    player styled

--- src/Components/Player/index.tsx
+++ src/Components/Player/index.tsx
@@ -1,3 +1,41 @@
+// @ts-nocheck
+import { useState } from 'react'
+import Marquee from "react-smooth-marquee"
+import './styles.css';
+
 export const Player = () => {
-    return <div>Player</div>
+    const [playing, setPlaying] = useState(false);
+    const [playPercentage, setPlayPercentage] = useState(30)
+
+    return <div className="player">
+        <p>
+            <div className="circle flashing" style={{backgroundColor:'red'}}/>
+            live now
+        </p>
+        <Marquee>
+            This is a long show title name
+        </Marquee>
+        <div 
+            onClick={() => {
+            setPlaying(!playing)
+            }}
+            className="playWrap"
+        >
+            {playing ? 'Playing' : "Paused"}
+        </div>
+        <div className="timeline">
+            <div className="marker" style={{left: `${playPercentage}%`}}/>
+        </div>
+        <div className="time">
+            1900 - 2000
+        </div>
+        <a className="watch">
+            Watch Live {'>'}
+        </a>
+        <div className="next">
+            <div>up next:</div>
+            <div>Show Name</div>
+            <div>1900 - 2000</div>
+        </div>
+    </div>
 }
