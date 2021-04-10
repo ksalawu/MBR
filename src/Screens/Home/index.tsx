753bb202ef35ec74f92475b7a70f363eb469f50b
Kieran Salawu <kieransalawu@kierans-mbp.home>
2021-04-10 18:24:44 +0100

    swap out for compressed video

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 export const Home = () => {
     return <div className="home">
         <video 
-            src='./videoHighRes.mp4'
+            src='./videoMidRes.mp4'
             autoPlay
             controls={false}
             loop
